7e2c06df910f07903aa4a07f09c4f0b476a7edff
Deepakmor07 <91567852+Deepakmor07@users.noreply.github.com>
2026-06-23 17:52:58 +0530

    Update output message in helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Guys, My name is Deepak");
-      System.out.println("Potugual will win the World Cup 2026");
+      System.out.println("Potugual will win the World Cup 2026 and Ronaldo Will score today on 23 June");
     }  
 } 
